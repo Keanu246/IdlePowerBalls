500be4b45908e418c820b61483b789395b6023c7
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2019-08-14 20:19:39 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ var clickBalls = [
   {active:false,x:700,y:200,dx:-.707,dy:.707,color:"#666666",radius:25,bDamage:10,rank:1,cost:10,id:103,clone:false,speed:4}, 
   {active:false,x:100,y:200,dx:-.707,dy:.707,color:"#666666",radius:25,bDamage:10,rank:1,cost:10,id:104,clone:false,speed:4}
 ];
-var balls = [ 
+var specialBalls = [ 
   {active:false,x:700,y:200,dx:.707,dy:.707,color:"#e25822",radius:10,bDamage:500000000000000000000,rank:0,cost:100000000000000000000000,id:0,clone:false,speed:1}, 
   {active:false,x:800,y:300,dx:.707,dy:.707,color:"#FFC21B",radius:15,bDamage:3000000000000000000000000,rank:0,cost:1000000000000000000000000,id:1,clone:false,speed:2}
 ]
